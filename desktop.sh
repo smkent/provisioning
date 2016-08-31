ed13930bda71a918caf0cceac9cff04d4085ea90
Stephen Kent <smkent@smkent.net>
2016-08-30 20:58:06 -0700

    Reliably detect Ansible installation and use the official PPA instead

--- desktop.sh
+++ desktop.sh
@@ -5,7 +5,9 @@ if [ "$(id -u)" -eq 0 ]; then
     exit 1
 fi
 
-if ! dpkg -l ansible >/dev/null 2>&1; then
+if ! which ansible >/dev/null 2>&1; then
+    sudo apt-add-repository -y ppa:ansible/ansible
+    sudo apt-get update
     sudo apt install -y ansible
 fi
 
